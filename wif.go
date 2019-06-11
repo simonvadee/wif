29c43792ba8d88d77279faf2546ae37393bc9939
Simon Vadée <simon@stratumn.com>
2019-06-11 15:25:10 +0200

    Add decode option

--- wif.go
+++ wif.go
@@ -3,19 +3,39 @@ package main
 import (
 	"log"
 	"fmt"
+	"flag"
 	"github.com/btcsuite/btcutil"
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg"
 )
 
+var existing string
+func init() {
+	flag.StringVar(&existing, "decode", "", "Existing WIF")
+	flag.Parse()
+}
+
 func main() {
-	sk, err := btcec.NewPrivateKey(btcec.S256())
-	if err != nil {
-		log.Fatal(err)
-	}
-	wif, err := btcutil.NewWIF(sk, &chaincfg.MainNetParams, false)
-	if err != nil {
-		log.Fatal(err)
+
+	var sk *btcec.PrivateKey
+	var wif *btcutil.WIF
+	var err error
+
+	if existing != "" {
+		wif, err = btcutil.DecodeWIF(existing)
+		if err != nil {
+			log.Fatal(err)
+		}
+		sk = wif.PrivKey
+	} else {
+		sk, err = btcec.NewPrivateKey(btcec.S256())
+		if err != nil {
+			log.Fatal(err)
+		}
+		wif, err = btcutil.NewWIF(sk, &chaincfg.MainNetParams, false)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	addr, err := btcutil.NewAddressPubKey(sk.PubKey().SerializeUncompressed(), &chaincfg.MainNetParams)
